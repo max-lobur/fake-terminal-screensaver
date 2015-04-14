ba073934fcc5a702cf9e8a77668965e6215a52ec
Max Lobur <max_lobur@outlook.com>
2015-04-13 17:37:50 -0700

    Open data file basing on script location instead of current

--- screen.py
+++ screen.py
@@ -1,9 +1,13 @@
 from __future__ import print_function	
+import os
 from random import randint
 from time import sleep
 
+my_location = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+input_path = os.path.join(my_location, 'nova-juno-tests-out')
+
 while True:
-    with open('nova-juno-tests-out') as f:
+    with open(input_path) as f:
         for l in f:
             print(l, end="")
             sleep(randint(100, 600) / 1000.0)   
